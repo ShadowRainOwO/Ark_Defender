567c3d6b080ff39c1e543911ec28d0cedef67b59
ShadowRainOwO <317862811@qq.com>
2026-08-12 16:52:06 +0800

    Update

--- Assets/C#Scripts/Camera/CameraFollow.cs
+++ Assets/C#Scripts/Camera/CameraFollow.cs
@@ -5,51 +5,35 @@ public class CameraFollow : MonoBehaviour
     [Header("跟随目标(玩家)")]
     public Transform target;
 
-
     [Header("摄像机距离")]
     public Vector3 offset = new Vector3(0, 5, -8);
 
-
     [Header("位置平滑速度")]
     public float moveSmoothSpeed = 8f;
-
 
     [Header("旋转设置")]
     public float rotateAngle = 45f;
     public float rotateSmoothSpeed = 8f;
 
-
     [Header("看向玩家高度")]
     public float lookHeight = 1.5f;
-
-
 
     private float currentYaw;
     private float targetYaw;
 
     private GameInput gameInput;
-
-
-
     void Awake()
     {
         gameInput = new GameInput();
     }
-
-
-
     void Start()
     {
         currentYaw = transform.eulerAngles.y;
         targetYaw = currentYaw;
     }
-
-
-
     void OnEnable()
     {
         gameInput.Player.Enable();
-
 
         // Q 左转45°
         gameInput.Player.RotateCamLeft.performed += _
@@ -57,7 +41,6 @@ public class CameraFollow : MonoBehaviour
         {
             targetYaw -= rotateAngle;
         };
-
 
         // E 右转45°
         gameInput.Player.RotateCamRight.performed += _
@@ -67,22 +50,15 @@ public class CameraFollow : MonoBehaviour
         };
     }
 
-
-
     void OnDisable()
     {
         gameInput.Player.Disable();
     }
 
-
-
-
     void LateUpdate()
     {
         if (target == null)
             return;
-
-
 
         // =========================
         // 1. E/Q控制水平旋转
@@ -94,8 +70,6 @@ public class CameraFollow : MonoBehaviour
             rotateSmoothSpeed * Time.deltaTime
         );
 
-
-
         Quaternion yawRotation =
             Quaternion.Euler(
                 0,
@@ -103,16 +77,11 @@ public class CameraFollow : MonoBehaviour
                 0
             );
 
-
-
         // =========================
         // 2. 计算摄像机位置
         // =========================
 
-        Vector3 desiredPosition =
-            target.position + yawRotation * offset;
-
-
+        Vector3 desiredPosition = target.position + yawRotation * offset;
 
         transform.position =
             Vector3.Lerp(
@@ -120,35 +89,16 @@ public class CameraFollow : MonoBehaviour
                 desiredPosition,
                 moveSmoothSpeed * Time.deltaTime
             );
-
-
-
         // =========================
         // 3. 看向玩家
         // 但是只改变上下角度
         // =========================
+        Vector3 lookTarget = target.position + Vector3.up * lookHeight;
+        Vector3 direction = lookTarget - transform.position;
 
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
 
-        Vector3 lookTarget =
-            target.position +
-            Vector3.up * lookHeight;
-
-
-
-        Vector3 direction =
-            lookTarget - transform.position;
-
-
-
-        Quaternion lookRotation =
-            Quaternion.LookRotation(direction);
-
-
-
-        float pitch =
-            lookRotation.eulerAngles.x;
-
-
+        float pitch = lookRotation.eulerAngles.x;
 
         // 保留E/Q控制的水平角
         transform.rotation =
